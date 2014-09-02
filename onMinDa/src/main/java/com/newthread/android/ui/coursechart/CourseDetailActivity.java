05c1443ee371eb4ab7a7010dfc132f8179bdf577
jindongping <jdp112233@qq.com>
2014-09-02 22:22:06 +0800

    设置 无课程 没有提醒

--- onMinDa/src/main/java/com/newthread/android/ui/coursechart/CourseDetailActivity.java
+++ onMinDa/src/main/java/com/newthread/android/ui/coursechart/CourseDetailActivity.java
@@ -2,7 +2,6 @@ package com.newthread.android.ui.coursechart;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,100 +14,101 @@ import com.actionbarsherlock.view.MenuItem;
 import com.newthread.android.R;
 import com.newthread.android.bean.SingleCourseInfo;
 import com.newthread.android.manager.CourseRemindManger;
-import com.newthread.android.ui.library.LibraryLoginActivity;
+import com.newthread.android.util.Loger;
 import com.newthread.android.util.StringUtils;
 
 public class CourseDetailActivity extends SherlockFragmentActivity {
-	private TextView courseName, teacher, place, time, duration;
-	private SingleCourseInfo info;
+    private TextView courseName, teacher, place, time, duration;
+    private SingleCourseInfo info;
     private Button remindButton;
-	private int position;
-	private int day;
-	@Override
-	protected void onCreate(Bundle arg0) {
-		super.onCreate(arg0);
-		setContentView(R.layout.activity_course_detail);
-		initData();
-		initView();
-	}
+    private int position;
+    private int day;
 
-	private void initData() {
-		info = (SingleCourseInfo) this.getIntent().getSerializableExtra(
-				"course_info");
-		position = this.getIntent().getExtras().getInt("position");
-		day = this.getIntent().getExtras().getInt("day");
-	}
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.activity_course_detail);
+        initData();
+        initView();
+    }
 
-	private void initView() {
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		ActionBar ab = getSupportActionBar();
-		ab.setDisplayHomeAsUpEnabled(true);
-		ab.setHomeButtonEnabled(true);
-		ab.setDisplayShowHomeEnabled(false);
+    private void initData() {
+        info = (SingleCourseInfo) this.getIntent().getSerializableExtra(
+                "course_info");
+        position = this.getIntent().getExtras().getInt("position");
+        day = this.getIntent().getExtras().getInt("day");
+    }
 
-		if (StringUtils.isEmpty(info.getCourseName())) {
-			ab.setTitle("课程详细");
-		} else {
-			ab.setTitle(info.getCourseName());
-		}
+    private void initView() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeButtonEnabled(true);
+        ab.setDisplayShowHomeEnabled(false);
 
-		courseName = (TextView) this.findViewById(R.id.course_name);
-		teacher = (TextView) this.findViewById(R.id.teacher);
-		place = (TextView) this.findViewById(R.id.place);
-		time = (TextView) this.findViewById(R.id.course_time);
-		duration = (TextView) this.findViewById(R.id.duration_weeks);
-        remindButton=(Button)this.findViewById(R.id.remindbutton);
+        if (StringUtils.isEmpty(info.getCourseName())) {
+            ab.setTitle("课程详细");
+        } else {
+            ab.setTitle(info.getCourseName());
+        }
+
+        courseName = (TextView) this.findViewById(R.id.course_name);
+        teacher = (TextView) this.findViewById(R.id.teacher);
+        place = (TextView) this.findViewById(R.id.place);
+        time = (TextView) this.findViewById(R.id.course_time);
+        duration = (TextView) this.findViewById(R.id.duration_weeks);
+        remindButton = (Button) this.findViewById(R.id.remindbutton);
         remindButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 CourseRemindManger.getInstance(getApplicationContext()).openRemind(info);
             }
         });
-        if(info==null){
+        if (info.getCourseName()==null||info.getCourseName().length()==0) {
             remindButton.setVisibility(View.GONE);
         }
-		if (!StringUtils.isEmpty(info.getCourseName())) {
-			courseName.setText(info.getCourseName());
-			teacher.setText(info.getTeacherName());
-			place.setText(info.getClassromNum());
-			time.setText(info.getNumOfDay());
-			duration.setText(info.getSustainTime());
-		} else {
-			courseName.setText("无");
-			teacher.setText("无");
-			place.setText("无");
-			time.setText("无");
-			duration.setText("无");
-		}
-	}
+        if (!StringUtils.isEmpty(info.getCourseName())) {
+            courseName.setText(info.getCourseName());
+            teacher.setText(info.getTeacherName());
+            place.setText(info.getClassromNum());
+            time.setText(info.getNumOfDay());
+            duration.setText(info.getSustainTime());
+        } else {
+            courseName.setText("无");
+            teacher.setText("无");
+            place.setText("无");
+            time.setText("无");
+            duration.setText("无");
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// TODO Auto-generated method stub
-		menu.add("编辑").setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        menu.add("编辑").setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			this.finish();
-			overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
-			break;
-					
-		}
-		if (item.getTitle().equals("编辑")) {
-			Intent intent = new Intent(getApplicationContext(),
-					CourseDetialEditActivity.class);
-			Bundle bundle = new Bundle();
-			bundle.putInt("day", day);
-			bundle.putInt("position", position);
-			intent.putExtras(bundle);
-			intent.putExtra("course_info", info);
-			CourseDetailActivity.this.startActivity(intent);
-			CourseDetailActivity.this.finish();
-		} 	
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
+                break;
+
+        }
+        if (item.getTitle().equals("编辑")) {
+            Intent intent = new Intent(getApplicationContext(),
+                    CourseDetialEditActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putInt("day", day);
+            bundle.putInt("position", position);
+            intent.putExtras(bundle);
+            intent.putExtra("course_info", info);
+            CourseDetailActivity.this.startActivity(intent);
+            CourseDetailActivity.this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
