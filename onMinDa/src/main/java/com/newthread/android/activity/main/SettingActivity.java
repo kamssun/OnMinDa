325e67272a6d38e05dcb84a0b6021d7f18b6ee30
jindongping <jdp112233@qq.com>
2014-09-03 14:47:04 +0800

    取消广告

--- onMinDa/src/main/java/com/newthread/android/activity/main/SettingActivity.java
+++ onMinDa/src/main/java/com/newthread/android/activity/main/SettingActivity.java
@@ -1,16 +1,10 @@
 package com.newthread.android.activity.main;
 
-import net.youmi.android.AdManager;
-import net.youmi.android.dev.AppUpdateInfo;
-import net.youmi.android.dev.CheckAppUpdateCallBack;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
@@ -80,55 +74,55 @@ public class SettingActivity extends SherlockActivity {
 	}
 
 	// 检查更新
-	public void checkAppUpdate() {
-		AdManager.getInstance(this).asyncCheckAppUpdate(
-				new CheckAppUpdateCallBack() {
-
-					@Override
-					public void onCheckAppUpdateFinish(
-							AppUpdateInfo appUpdateInfo) {
-						if (appUpdateInfo == null) {
-							Toast.makeText(SettingActivity.this, "当前版本已经是最新版",
-									Toast.LENGTH_SHORT).show();
-						} else {
-							// 获取版本号
-							int versionCode = appUpdateInfo.getVersionCode();
-							// 获取版本
-							String versionName = appUpdateInfo.getVersionName();
-							// 获取新版本的信息
-							String updateTips = appUpdateInfo.getUpdateTips();
-							// 获取apk下载地址
-							final String downloadUrl = appUpdateInfo.getUrl();
-
-							AlertDialog updateDialog = new AlertDialog.Builder(
-									SettingActivity.this)
-									.setIcon(android.R.drawable.ic_dialog_info)
-									.setTitle("发现新版本 " + versionName)
-									.setMessage(updateTips)
-									.setPositiveButton(
-											"更新",
-											new DialogInterface.OnClickListener() {
-												@Override
-												public void onClick(
-														DialogInterface dialog,
-														int which) {
-													try {
-														Intent intent = Intent
-																.parseUri(
-																		downloadUrl,
-																		Intent.FLAG_ACTIVITY_NEW_TASK);
-														startActivity(intent);
-													} catch (Exception e) {
-														// exception
-													}
-												}
-											}).setNegativeButton("下次再说", null)
-									.create();
-							updateDialog.show();
-						}
-					}
-				});
-	}
+//	public void checkAppUpdate() {
+//		AdManager.getInstance(this).asyncCheckAppUpdate(
+//				new CheckAppUpdateCallBack() {
+//
+//					@Override
+//					public void onCheckAppUpdateFinish(
+//							AppUpdateInfo appUpdateInfo) {
+//						if (appUpdateInfo == null) {
+//							Toast.makeText(SettingActivity.this, "当前版本已经是最新版",
+//									Toast.LENGTH_SHORT).show();
+//						} else {
+//							// 获取版本号
+//							int versionCode = appUpdateInfo.getVersionCode();
+//							// 获取版本
+//							String versionName = appUpdateInfo.getVersionName();
+//							// 获取新版本的信息
+//							String updateTips = appUpdateInfo.getUpdateTips();
+//							// 获取apk下载地址
+//							final String downloadUrl = appUpdateInfo.getUrl();
+//
+//							AlertDialog updateDialog = new AlertDialog.Builder(
+//									SettingActivity.this)
+//									.setIcon(android.R.drawable.ic_dialog_info)
+//									.setTitle("发现新版本 " + versionName)
+//									.setMessage(updateTips)
+//									.setPositiveButton(
+//											"更新",
+//											new DialogInterface.OnClickListener() {
+//												@Override
+//												public void onClick(
+//														DialogInterface dialog,
+//														int which) {
+//													try {
+//														Intent intent = Intent
+//																.parseUri(
+//																		downloadUrl,
+//																		Intent.FLAG_ACTIVITY_NEW_TASK);
+//														startActivity(intent);
+//													} catch (Exception e) {
+//														// exception
+//													}
+//												}
+//											}).setNegativeButton("下次再说", null)
+//									.create();
+//							updateDialog.show();
+//						}
+//					}
+//				});
+//	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
